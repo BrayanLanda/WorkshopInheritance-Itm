a185554832c2fc4dbf29ca7fb577fd73b43aad7e
Brayan Landa <landakkpp@gmail.com>
2026-09-05 23:37:10 -0500

    feat: Add Parallelogram and Triangle classes with area and perimeter methods; update Program.cs to include instances of both classes

---
+++ Backend/Parallelogram.cs
@@ -0,0 +1,34 @@
+namespace Backend;
+
+public class Parallelogram : Rectangle
+{
+    private double _h;
+
+    public Parallelogram(string name, double a, double b, double h) : base(name, a, b)
+    {
+        H = h;
+    }
+
+    public double H
+    {
+        get => _h;
+        set
+        {
+            if (!ValidateH(value))
+                throw new ArgumentException("Height must be greater than 0.");
+            _h = value;
+        }
+    }
+
+    public override double GetArea()
+    {
+        return A * H;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (A + B);
+    }
+
+    private bool ValidateH(double h) => h > 0;
+}
---
+++ Backend/Triangle.cs
@@ -0,0 +1,34 @@
+namespace Backend;
+
+public class Triangle : Rectangle
+{
+    private double _c;
+    private double _h;
+    public Triangle(string name, double a, double b, double c, double h) : base(name, a, b)
+    {
+        C = c;
+        H = h;
+    }
+    public double C { 
+        get => _c; 
+        set
+        {
+            if (!ValidateC(value))
+                throw new ArgumentException("Side C must be greater than 0.");
+            _c = value;
+        }
+    }
+    public double H { 
+        get => _h; 
+        set
+        {
+            if (!ValidateH(value))
+                throw new ArgumentException("Height must be greater than 0.");
+            _h = value;
+        }
+    }
+    public override double GetArea() => (A * H) / 2;
+    public override double GetPerimeter() => A + B + C;
+    private bool ValidateC(double c) => c > 0;
+    private bool ValidateH(double h) => h > 0;
+}
